409c0fe49a2487ce6ec120bfacc9f75d7279ee2c
yuriongit <yuri.dev44@outlook.com>
2026-04-11 01:07:24 -0400

    Add `DELETE /todos`, fix type-structures + redundancy, implement further type-safety Details:    Feat:       Add `DELETE /todos`, complete Controller/Service/Repo,       Create generic umbrella return type to avoid typos + to gain auto-completion    Biome:       Lint,       Format       Reorganize imports    Notes:       Moved types to Controller file

--- backend/src/repo/todo.repo.ts
+++ backend/src/repo/todo.repo.ts
@@ -2,26 +2,31 @@ import { supabase } from "@app/supabase/client"
 import { ErrorHandler } from "@/middleware/error/errorHandler"
 import type { CreateTodo, Todo } from "@/services/todo.service"
 
-type InsertTodoResponse = {
-	message: string
+type TodoResponse<TAction extends "created" | "retrieved" | "updated" | "deleted"> = {
+	message: `To-do ${TAction} successfully`
+	todo: Todo
 }
+type InsertTodoResponse = TodoResponse<"created">
+type QueryTodoResponse = TodoResponse<"retrieved">
+type DeleteTodoResponse = TodoResponse<"deleted">
 
 export const TodoRepo = {
 	async insert(todo: CreateTodo): Promise<InsertTodoResponse> {
-		const { error } = await supabase.from("todo_items").insert(todo)
+		const { data, error } = await supabase.from("todo_items").insert(todo).select("*").single()
 
 		if (error) {
-			throw new ErrorHandler(403, {
+			throw new ErrorHandler(404, {
 				details: error.details,
 				message: error.message,
 			})
 		}
 
 		return {
-			message: `To-do created succesfully`,
+			message: `To-do created successfully`,
+			todo: data,
 		}
 	},
-	async query(todoId: number): Promise<Todo> {
+	async query(todoId: number): Promise<QueryTodoResponse> {
 		const { data: todo, error } = await supabase
 			.from("todo_items")
 			.select("*")
@@ -34,6 +39,33 @@ export const TodoRepo = {
 				message: `To-do does not exist`,
 			})
 		}
-		return todo
+		return {
+			message: "To-do retrieved successfully",
+			todo: todo,
+		}
+	},
+	async update(parameter: unknown): Promise<void> {
+		parameter
+		return
+	},
+	async delete(todoId: number): Promise<DeleteTodoResponse> {
+		const { data: todo, error } = await supabase
+			.from("todo_items")
+			.delete()
+			.eq("id", todoId)
+			.select()
+			.single()
+
+		if (error) {
+			throw new ErrorHandler(404, {
+				details: error.details,
+				message: `To-do does not exist`,
+			})
+		}
+
+		return {
+			message: `To-do deleted successfully`,
+			todo: todo,
+		}
 	},
 }
